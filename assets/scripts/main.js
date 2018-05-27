aafcc4ea47fc4c54fa2a7c47444fefff493efe97
Grégori Sória <gregori.soria@base.digital>
2018-05-27 20:13:16 -0300

    red button; button spacement; cpf validation;

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -22,8 +22,42 @@ var pageCpf = {
 			if (!cpf.validity.valid) {
 				cpf.setCustomValidity('Campo inválido');
 			}
+
+			var val = cpf.value;
+
+			val = val.replace(/[^0-9.]+/g, '');
+
+			cpf.value = val;
 		});
-	}
+
+		cpf.addEventListener('keydown', function(e) {
+			console.log(e.keyCode);
+			if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 return;
+			}
+			if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+				e.preventDefault();
+			}
+
+			var val = cpf.value;
+
+			val = val.replace(/[^0-9.]+/g, '');
+
+			cpf.value = val;
+		});
+
+		cpf.addEventListener('blur', function(e) {
+			var val = cpf.value;
+
+			val = val.replace(/[^0-9.]+/g, '');
+
+			cpf.value = val;
+		});
+	},
 };
 
 var pagePayment = {
