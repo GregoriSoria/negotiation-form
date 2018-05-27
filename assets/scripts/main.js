69d00ed080acb9935134cf5ce20c8fb85c7555d9
Grégori Sória <gregori.soria@base.digital>
2018-05-27 05:13:01 -0300

    finish

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,94 @@
 (function() {
-var page = {
+
+var pageCpf = {
 	init: function() {
-		console.log('Page Start!');
 		this.declaracoes();
 	},
 
 	declaracoes: function() {
-		this.clickTitulo();
+		this.validateForm();
 	},
 
-	clickTitulo: function() {
-		
+	validateForm: function() {
+		this.setCustomValidity();
 	},
+
+	setCustomValidity: function() {
+		var cpf = document.querySelector('[name=cpf]');
+		cpf.setCustomValidity('Campo inválido');
+		cpf.addEventListener('keyup', function(e) {
+			cpf.setCustomValidity('');
+
+			if (!cpf.validity.valid) {
+				cpf.setCustomValidity('Campo inválido');
+			}
+		});
+	}
 };
 
-page.init();
+var pagePayment = {
+	init: function() {
+		this.declaracoes();
+	},
+
+	declaracoes: function() {
+		this.validateForm();
+	},
+
+	validateForm: function() {
+		this.setCustomValidity();
+	},
+
+	setCustomValidity: function() {
+		var email = document.querySelector('[name=email]');
+		email.addEventListener('keyup', function(e) {
+			if (email) {
+				email.setCustomValidity('');
+				
+				if (!email.validity.valid) {
+					email.setCustomValidity('Email inválido');
+				}
+			}
+		});
+	}
+};
+
+var pageConfirm = {
+	init: function() {
+		this.declaracoes();
+	},
+
+	declaracoes: function() {
+		this.backButton();
+	},
+
+	backButton: function() {
+		var buttons = document.querySelectorAll('button.back');
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].addEventListener('click', function(e) {
+				e.preventDefault();
+				window.history.back();
+			});
+		}
+	}
+};
+
+var main = document.querySelector('.main');
+if (main) {
+	var step = main.dataset.step;
+	console.log('Step: ', step);
+	switch (step) {
+		case 'start':
+		break;
+		case 'cpf':
+			pageCpf.init();
+		break;
+		case 'payment':
+			pagePayment.init();
+		break;
+		case 'confirm':
+			pageConfirm.init();
+		break;
+	}
+}
 })(window);
